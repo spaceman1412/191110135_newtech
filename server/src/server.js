073c28028f57a4b224b1922336cb76f7b727d85a
Thien Nguyen <spacemannormal@gmail.com>
2023-10-27 18:23:43 +0700

    aa

---
+++ server/src/server.js
@@ -0,0 +1,19 @@
+const http = require("http");
+
+const app = require("./app");
+
+const PORT = process.env.PORT || 8000;
+
+const { loadPlanetsData } = require("./models/planets.model");
+
+const server = http.createServer(app);
+
+async function startServer() {
+  await loadPlanetsData();
+
+  server.listen(PORT, () => {
+    console.log("Listening on port " + PORT);
+  });
+}
+
+startServer();
